5411969e77589b4d653e6991bd67d3b6fbe0d0d3
Juanma <Juanma888@gmail.com>
2024-12-04 21:45:32 +0100

    Me da problemas el import. (Sin CDN, probando otras formas de usar jQuery en el proyecto). Añadir type="module" al <script> tampoco funciona.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,17 @@
+
+import $ from 'jquery';
+
+$(document).ready(function() {
+    $("#boton").click(function() {
+        alert('Me ha presionado... me voy de vacaciones a relajarme');
+    });
+});
+
+
+/*
 document.getElementById('boton').onclick = function() {
     alert('Me ha presionado... me voy de vacaciones a relajarme');
 }
+*/
 
 console.log('Hola mundo... por tercera vez');
