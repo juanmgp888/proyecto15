629589263db9aea54a641aaf532fe05c2c4bb8e3
curso04 <curso04@gmail.com>
2024-12-05 10:43:14 +0100

    $import no hacía falta y me daba problemas. Por otro lado, jquery ya estaba en node_modules. No hacía falta descargármelo y guardarlo en \javascript

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,18 +1,19 @@
-/*
-import $ from 'jquery';
+
+//import $ from 'jquery';
 
 $(document).ready(function() {
     $("#boton").click(function() {
         alert('Me has pulsado... me voy de vacaciones a relajarme');
     });
 });
-*/
 
 
+/*
 document.getElementById('boton').onclick = function() {
 document.getElementById('boton').innerText ="Ya me has pulsado";
+// Más legible: this.innerText="Ya me has pulsado"
 //    alert('Me ha presionado... me voy de vacaciones a relajarme');
 }
-
+*/
 
 console.log('Hola mundo... por tercera vez');
