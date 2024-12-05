7aff061d85eae2816dd93765c4911e33cc8d46eb
curso04 <curso04@gmail.com>
2024-12-05 11:29:38 +0100

    Más pruebas de los anterior

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,19 +1,46 @@
 
-//import $ from 'jquery';
+//import $ from 'jquery';  Se usa con 'modules', queno vamos a ver en el curso.
+// Un elemento superpone al otro. Si disparan seguidos y no nos da tiempo a ver uno de ellos salvo 
+// usando el depurador.
+/*
+
+ // DIFERENTES MANERAS DE CAMBIAR EL TEXTO DENTRO DE UN BOTÓN Y DE PREPARAR ESE COMPORTAMIENTO:
+*/
 
 $(document).ready(function() {
+
+/*
     $("#boton").click(function() {
-        alert('Me has pulsado... me voy de vacaciones a relajarme');
+        $('#boton').text('Me has pulsado... me voy de vacaciones a relajarme un poquito.');
     });
-});
+*/
+
+
+
+/*
+$("button").on("click", function(event){
+    $(this).text("Otro mensajito");
+})
+*/
+
+// document.getElementById('boton').innerText ="Ya me has pulsado------S";
 
 
 /*
 document.getElementById('boton').onclick = function() {
-document.getElementById('boton').innerText ="Ya me has pulsado";
-// Más legible: this.innerText="Ya me has pulsado"
-//    alert('Me ha presionado... me voy de vacaciones a relajarme');
-}
 */
 
+// Más legible: this.innerText="Ya me has pulsado"
+//    alert('Me ha presionado... me voy de vacaciones a relajarme');
+//}
+
+accionParaElClick = function(event){
+    $(this).text("Bye bye");
+}
+$("button").on("click",accionParaElClick); // Debajo del accionParaElClick
+
+
 console.log('Hola mundo... por tercera vez');
+
+
+});
