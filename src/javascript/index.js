4b0a445663d5f770326e7f725ec9d59ce62cebbd
curso04 <curso04@gmail.com>
2024-12-05 09:12:23 +0100

    Donde lo dejé

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,16 @@
-console.log('Hola mundo... por segunda vez');
+import $ from 'jquery';
+
+$(document).ready(function() {
+    $("#boton").click(function() {
+        alert('Me has pulsado... me voy de vacaciones a relajarme');
+    });
+});
+
+
+/*
+document.getElementById('boton').onclick = function() {
+    alert('Me ha presionado... me voy de vacaciones a relajarme');
+}
+*/
+
+console.log('Hola mundo... por tercera vez');
