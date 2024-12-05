9e7be6e34e40d86b7b5f9769de4ce3677b0e2b89
curso04 <curso04@gmail.com>
2024-12-05 10:28:48 +0100

    Alterar texto del btn usando innerText

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
 document.getElementById('boton').onclick = function() {
 document.getElementById('boton').innerText ="Ya me has pulsado";
-    alert('Me ha presionado... me voy de vacaciones a relajarme');
+//    alert('Me ha presionado... me voy de vacaciones a relajarme');
 }
 
 
